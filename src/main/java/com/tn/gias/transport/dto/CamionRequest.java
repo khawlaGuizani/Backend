339f8b9597d75253965275433dff60c73eee19d4
khawlaGuizani <khawlaguizani70@gmail.com>
2026-07-23 17:21:36 +0000

    fix(security): externalize mail credentials + add H2 for  tests

--- src/main/java/com/tn/gias/transport/dto/CamionRequest.java
+++ src/main/java/com/tn/gias/transport/dto/CamionRequest.java
@@ -3,10 +3,11 @@ import lombok.Data;
 
 @Data
 public class CamionRequest {
+    //les attributs de la classe CamionRequest
     private String immatriculation;
     private boolean disponible;
     private double capaciteReelle;
-    private int annee;
+    //private int annee;
 
     private Long typeCamionId;
 }
